3e76985c75754c16c9a1422bb2cfc1b68d117761
felipecarpes <fcarpes@imply.com>
2023-03-05 14:57:26 -0300

    implement characters query

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,8 +1,14 @@
 import gql from 'graphql-tag'
 
 export const CHARACTERS_QUERY = gql`
-  query {
-    characters(page: 1, filter: {}) {
+  query ($page: Int, $filterName: String!) {
+    characters(page: $page, filter: { name: $filterName }) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
       results {
         id
         name
