ecf990a48ff4f2d18046e5891de55ac999738aed
felipecarpes <fcarpes@imply.com>
2023-03-05 18:20:40 -0300

    creating new route to character

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,14 @@ const history = createWebHistory()
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
+    name: 'Home',
     component: () => import('../pages/Home.vue'),
   },
+  {
+    path: '/character/:id',
+    name: 'Character',
+    component: () => import('../pages/Character.vue'),
+  }
 ]
 
 const router = createRouter({
