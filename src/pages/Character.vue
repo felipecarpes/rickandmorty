ed831a5afb8e75518b092fd1c94dcce76b1fd842
felipecarpes <fcarpes@imply.com>
2023-03-05 19:19:39 -0300

    fix character page

--- src/pages/Character.vue
+++ src/pages/Character.vue
@@ -1,44 +1,27 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useQuery } from "@vue/apollo-composable"
-import { CHARACTER_QUERY } from '../graphql/query'
-import { useRoute, useRouter } from "vue-router"
+  import { computed } from 'vue'
+  import { useQuery } from "@vue/apollo-composable"
+  import { CHARACTER_QUERY } from '../graphql/query'
+  import { useRoute } from "vue-router"
+  import BackButton from '../components/BackButton.vue'
 
-const route = useRoute()
-const router = useRouter()
+  const route = useRoute()
 
-const { result, loading, error, refetch } = useQuery(CHARACTER_QUERY, 
-  () => ({
-    "id": Number(route.params.id)
-  })
-)
+  const { result, loading, error, refetch } = useQuery(CHARACTER_QUERY, 
+    () => ({
+      "id": Number(route.params.id)
+    })
+  )
 
-const character = computed(() => result.value?.character)
+  const character = computed(() => result.value?.character)
 
-const loadingCharacter = computed(() => loading.value)
-
-const routerToHomePage = () => {
-  router.push({
-    name: 'Home'
-  })
-}
-
+  const loadingCharacter = computed(() => loading.value)
 </script>
 <template>
-  <span @click="routerToHomePage()" class="back-button">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M10.854 1.146a.5.5 0 0 1 0 .708L6.707 6H13.5a.5.5 0 0 1 0 1H6.707l4.147 4.146a.5.5 0 1 1-.708.708l-5-5a.5.5 0 0 1 0-.708l5-5a.5.5 0 0 1 .708 0z"/>
-    </svg>
-    back 
-  </span>
+  <BackButton />
   <h1 v-if="loadingCharacter">Loading...</h1>
   <div class="character-page" v-else>
-    <span @click="routerToHomePage()" class="back-button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M10.854 1.146a.5.5 0 0 1 0 .708L6.707 6H13.5a.5.5 0 0 1 0 1H6.707l4.147 4.146a.5.5 0 1 1-.708.708l-5-5a.5.5 0 0 1 0-.708l5-5a.5.5 0 0 1 .708 0z"/>
-      </svg>
-      back 
-    </span>
+    <BackButton />
     <div class="page-header">
       <h1>{{ character?.name }}</h1>
     </div>
@@ -128,6 +111,10 @@ const routerToHomePage = () => {
   .episodes-list {
     list-style: none;
     padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
   }
 
   .episodes-list li {
@@ -156,23 +143,6 @@ const routerToHomePage = () => {
 
   span:hover {
     color: #3498DB;
-  }
-
-  .back-button {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-bottom: 1rem;
-    line-height: 1;
-    padding-bottom: 1rem;
-  }
-
-  .back-button svg {
-    margin-right: 0.5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 5px;
   }
 
   @media (max-width: 768px) {
